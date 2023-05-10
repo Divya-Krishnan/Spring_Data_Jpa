cbbe559ec46cbbe902f6aebc8f6acab915a7f7f5
Divya-Krishnan <dkrishnan384@gmail.com>
2023-05-10 17:00:35 +0530

    JPA - delete Data

---
+++ JPA-Delete-Data/src/main/java/com/divya/database/databasedemo/jpa/PersonJPARepository.java
@@ -0,0 +1,49 @@
+package com.divya.database.databasedemo.jpa;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.divya.database.databasedemo.entity.Person;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
+public class PersonJPARepository {
+
+	//connect to Database
+	//Manages entities  - All operations are going through persistance context
+	@PersistenceContext
+	EntityManager entityManager;
+	public Person findById(int id)
+	{
+		return entityManager.find(Person.class, id);
+				
+				
+	}
+	//Update
+	public Person update(Person person)
+	{
+		return entityManager.merge(person);
+				
+				
+	}
+	//Insert
+		public Person insert(Person person)
+		{
+			return entityManager.merge(person);
+					
+					
+		}
+		
+		//delete
+		public void delete(int id)
+		{
+			Person person=findById(id);
+		entityManager.remove(person);
+		
+		}
+	
+}
